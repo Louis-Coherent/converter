ee32120002b2a265d5ea2011b46fafa1e0927549
Louis-Coherent <louis@coherent-innovations.co.uk>
2025-02-26 20:21:54 +0000

    Log ClamAV scan command and results for improved error tracking

--- app/Controllers/File.php
+++ app/Controllers/File.php
@@ -166,7 +166,8 @@ class File extends Controller
 
         $scanResult = shell_exec(escapeshellarg($clamdPath) . " --no-summary " . escapeshellarg($filePath));
 
-        log_message('error', $scanResult);
+        log_message('info', escapeshellarg($clamdPath) . " --no-summary " . escapeshellarg($filePath));
+        log_message('error', json_encode($scanResult));
 
         if (strpos($scanResult, 'OK') === false) {
             if (file_exists($filePath)) {
