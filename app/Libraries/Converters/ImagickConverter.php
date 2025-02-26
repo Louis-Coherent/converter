fc463ecca781380cf368487eac07a8b9a1bf2c4f
Louis-Coherent <louis@coherent-innovations.co.uk>
2025-02-26 19:22:37 +0000

    Add file corruption check in ImagickConverter using Ghostscript

--- app/Libraries/Converters/ImagickConverter.php
+++ app/Libraries/Converters/ImagickConverter.php
@@ -4,6 +4,8 @@ namespace App\Libraries\Converters;
 
 use Exception;
 
+use function PHPUnit\Framework\throwException;
+
 class ImagickConverter implements ConverterInterface
 {
     public static function getSupportedConversions(): array
@@ -29,6 +31,16 @@ class ImagickConverter implements ConverterInterface
 
         if (!in_array($to, self::getSupportedConversions()[$from])) {
             throw new Exception("Unsupported output format: $to");
+        }
+
+        $cmd = "gs -o /dev/null -sDEVICE=nullpage -dSAFER " . escapeshellarg($filePath);
+        $output = [];
+        $returnVar = 0;
+
+        exec($cmd, $output, $returnVar);
+
+        if ($returnVar !== 0) {
+            throw new Exception("Corrupt file.");
         }
 
         $imagick = new \Imagick();
